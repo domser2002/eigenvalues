1f4afd829023e517f457cddf549e923e5fc8e1d0
domser2002 <domser2002@o2.pl>
2023-01-05 22:11:16 +0100

    in progress

--- P2Z33_DSE_eigenvalues.m
+++ P2Z33_DSE_eigenvalues.m
@@ -20,25 +20,39 @@ arguments
     kmax=100;
 end
 %warning('off', 'all')
+n=size(A,1);
 m=max(max(A));
-x0=m*rand(size(A,1),1);
-lambda=m*rand(size(A,1),1);
-err=1;
-k=0;
-while k<kmax
-    if err<tol
+x0=m*rand(n,1);
+lambda=zeros(1,n);
+err=ones(1,n);
+k=1;
+i=1;
+x=x0;
+while true
+    while k<kmax
+        if err(i)<tol
+            break;
+        end
+        % Compute the new approximation of the eigenvector
+        y = A*x;
+        % Compute the new approximation of the eigenvalue
+        lambdaNew = (y'*x)/(x'*x);
+        % Compute the error between the old and new approximations of the eigenvalue
+        err(i) = abs(lambda(i) - lambdaNew);
+        % Update the variables
+        lambda(i) = lambdaNew;
+        x = y/norm(y);
+        k=k+1;
+    end
+    if k==kmax
         break;
     end
-    % Compute the new approximation of the eigenvector
-    y = A*x0;
-    % Compute the new approximation of the eigenvalue
-    lambdaNew = (y'*x0)/(x0'*x0);
-    % Compute the error between the old and new approximations of the eigenvalue
-    err = abs(lambda - lambdaNew);
-    % Update the variables
-    lambda = lambdaNew;
-    x0 = y/norm(y);
-    k=k+1;
+    % Deflate the matrix A
+    A = A - lambda(i)*(x*x');
+    % Reset variables
+    x = x0;
+    k = 1;
+    i=i+1;
 end
 E=lambda;
 end
